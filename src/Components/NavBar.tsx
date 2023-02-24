aa1dbb989c533a5ffa6b06f57eee33f4735e86ac
shanmugam145 <shanmugam@authorify.com>
2023-02-24 12:35:45 +0530

    class-issue

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -21,13 +21,13 @@ const NavBar = () => {
         <div className='md:hidden lg:hidden'>
           {!toggle? <svg xmlns="http://www.w3.org/2000/svg" 
                     onClick={() => setToggle(true)}  
-                    fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 cursor-pointer">
+                    fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 cursor-pointer">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg>
                     : 
                     <svg xmlns="http://www.w3.org/2000/svg" 
                     onClick={() => setToggle(false)}
-                    fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 cursor-pointer float-right">
+                    fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 cursor-pointer float-right">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                     </svg>
           }
