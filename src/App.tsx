ea469acf229b92672fb7639e94812932313c9667
shanmugam145 <shanmugam@authorify.com>
2023-03-06 22:07:22 +0530

    changes-in-UI-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Provider } from 'react-redux';
+
 import './App.css'
 import NavBar from './Components/NavBar'
 import Home from './Components/Home'
@@ -7,19 +7,13 @@ import Skills from './Components/Skills'
 import Footer from './Components/Footer'
 import zipy from "zipyai";
 import Portfolio from './Components/Portfolio'
-import CakeContainer from './ReduxComponent/CakeContainer'
-import store from './redux/store';
-import HooksCakeContainer from './ReduxComponent/HooksCakeContainer';
 zipy.init('c0e9302d');
 
 function App() {
   return (
-    <Provider store={store}>
     <div className='p-[20px]'>
       {/* Nav Bar */}
       <NavBar />
-      {/* <CakeContainer />
-      <HooksCakeContainer /> */}
       {/* Intro */}
       <Home />
       {/* About Me */}
@@ -33,7 +27,6 @@ function App() {
       {/* Footer */}
       <Footer />
     </div>
-    </Provider>
   )
 }
 
--- src/redux/store.ts
+++
@@ -1,7 +0,0 @@
-import { createStore } from 'redux';
-import cakeReducers from './cake/cakeReducers';
-
-const store = createStore(cakeReducers);
-
-export default store; 
-
